3d5c2149221603084684bf07ed3681267bcbb517
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2023-08-22 23:19:30 +0530

    length of the longest valid substring

---
+++ Stack/findMaxLen.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve(){
+  string s ;
+  cin >> s;
+  int n= s.length();
+  stack<int> st;
+  st.push(-1);
+  int r = 0 ;
+
+  for(int i=0; i<n; i++){
+    if(s[i]=='(') st.push(i);
+    else{
+      if(!st.empty())
+        st.pop();
+    }
+    if(!st.empty())
+      r = i-st.top() > r ? i-st.top() : r;
+    else
+      st.push(i);
+  }
+  cout << r << endl;
+}
+
+int main(){
+  ll tc;
+  cin >> tc;
+  while(tc--) solve();
+}
