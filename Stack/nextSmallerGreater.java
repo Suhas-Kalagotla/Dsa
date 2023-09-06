a772137085ee59a4727b2cebd16e968f38017786
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2023-09-06 23:31:06 +0530

    printing right smaller of next greater

--- Stack/nextSmallerGreater.java
+++ Stack/nextSmallerGreater.java
@@ -9,30 +9,48 @@ class NextSmallerGreater{
     for(int i =0; i<n; i++) {
       a[i]=sc.nextInt();
     }
-    
+
     System.out.println();
     Stack <Integer> st = new Stack<>();
     int greater[] = new int[n];
     int smaller[] = new int[n];
 
     for(int i =0 ;i <n; i++){
-      if(st.empty() || (a[st.peek()] > a[i])) st.push(i);
-      else{
         while(!st.empty() && a[st.peek()] < a[i]){
         int val = st.peek();
         greater[val] = i;
         st.pop();
         }
-      }
+        st.push(i);
     }
     while(!st.empty()){
       int val = st.peek();
       greater[val]  = -1;
       st.pop();
     }
-    for(int i=0; i<n; i++){
-      System.out.print(greater[i] +  " ") ;
+
+    for(int i=0;i <n; i++){
+      while(!st.empty() && a[st.peek()]>a[i]){
+        int val = st.peek();
+        smaller[val] = i;
+        st.pop();
+      }
+      st.push(i);
     }
+    while(!st.empty()){
+      int val = st.peek();
+      smaller[val] = -1;
+      st.pop();
+    }
+
+     for(int i=0;i <n; i++){
+       if(greater[i]==-1 || smaller[greater[i]]==-1){
+         System.out.print(-1 + " ");
+       }else{
+         System.out.print(a[smaller[greater[i]]]+" ");
+       }
+     }
+
 
   }
 }
