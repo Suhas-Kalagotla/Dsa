d616ac88ce5e8cc05c843c64fbe69476449ad1eb
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2023-09-10 14:48:28 +0530

    print yes if k can be made ideal point or no

---
+++ Codeforces/1795b-idealPoint.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while (t--) {
+		int n,l,r, a=0, b=0, k; cin>>n>>k;
+		while (n--) {
+			cin>>l>>r;
+			if (l==k) a=1;
+			if(r==k) b=1;
+		}
+		cout<<(a&b ? "YES\n" : "NO\n");
+	}
+}
